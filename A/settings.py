c2d7495609f800c7b87bb553c334d7f3a8ab7a77
milad-sol <lvlilad_soltani@yahoo.com>
2025-02-21 12:06:02 +0330

    create include directory and add messages and nvabr

--- A/settings.py
+++ A/settings.py
@@ -35,7 +35,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # LOCAL APPS
     'home.apps.HomeConfig',
+
+    # THIRD PARTY
+    'bootstrap5',
+
 ]
 
 MIDDLEWARE = [
