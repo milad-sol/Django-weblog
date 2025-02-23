6e981899b3acb1d315de47ecb64227dd6c6595d7
milad-sol <lvlilad_soltani@yahoo.com>
2025-02-23 11:53:29 +0330

    start to create login and register page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+class RegisterView(TemplateView):
+    template_name = 'accounts/register.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'accounts/login.html'
